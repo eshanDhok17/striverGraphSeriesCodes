7e98d207355348fd5a3652a0fe65bce51d376978
eshanDhok17 <95692413+eshanDhok17@users.noreply.github.com>
2022-12-11 22:32:09 +0530

    Add files via upload

---
+++ G-26 Alien Dictionary.cpp
@@ -0,0 +1,53 @@
+// Alien Dictionary
+// Toposort
+
+class Solution{
+  private:
+    vector<int> topoSort(int n, vector<int> adj[])  {
+	    vector<int> topo, indegree(n, 0);
+	    for(int i=0; i<n; i++) {
+	        for(int &it : adj[i]) {
+	            indegree[it]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0; i<n; i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i);
+	        }
+	    }
+	    while(!q.empty()) {
+	        int node = q.front(); q.pop();
+	        topo.push_back(node);
+	        for(int &it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it);
+	            }
+	        }
+	    }
+	    return topo;
+	}
+	
+  public:
+    string findOrder(string dict[], int N, int K) {
+        vector<int> adj[K];
+        for(int i=0; i<N-1; i++) {
+            string s1 = dict[i], s2 = dict[i+1];
+            int size = min(s1.size(), s2.size());
+            for(int j=0; j<size; j++) {
+                if(s1[j] != s2[j]) {
+                    adj[s1[j] - 'a'].push_back(s2[j] - 'a');
+                    break;
+                }
+            }
+        }
+        
+        vector<int> topo = topoSort(K, adj);
+        string ans = "";
+        for(auto it : topo) {
+            ans += char(it + 'a');
+        }
+        return ans;
+    }
+};
