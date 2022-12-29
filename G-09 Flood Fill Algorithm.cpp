97b861270ac3f7b2c4d8d4e08784c85798cfc27b
eshanDhok17 <95692413+eshanDhok17@users.noreply.github.com>
2022-12-29 12:58:08 +0530

    Update G-09 Flood Fill Algorithm.cpp

--- G-09 Flood Fill Algorithm.cpp
+++ G-09 Flood Fill Algorithm.cpp
@@ -1,5 +1,5 @@
 // G-9 Flood Fill Algorithm
-
+// GFG
 class Solution {
 public:
     vector<vector<int>> res;
@@ -30,6 +30,41 @@ public:
         int val =  image[sr][sc];
         res[sr][sc] = newColor;
         bfs(sr, sc, val, newColor, image);
+        return res;
+    }
+};
+
+
+// LeetCode
+
+class Solution {
+public:
+    void dfs(int row, int col, int &color, int &val, vector<vector<bool>> &vis, vector<vector<int>> &img, vector<vector<int>> &res) {
+        int n = size(img), m = size(img[0]);
+        vis[row][col] = true;
+        res[row][col] = color;
+        for(int i=-1; i<=1; i++)
+        {
+            for(int j=-1; j<=1; j++)
+            {
+                if(abs(i) == abs(j)) continue;
+                else
+                {
+                    int nr = row+i, nc = col+j;
+                    if(nr>=0 and nc>=0 and nr<n and nc<m and img[nr][nc] == val and !vis[nr][nc])
+                    {
+                        dfs(nr, nc, color, val, vis,img, res);
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
+        int val = img[sr][sc];
+        int n = size(img), m = size(img[0]);
+        vector<vector<bool>> vis(n, vector<bool> (m, false));
+        vector<vector<int>> res = img;
+        dfs(sr, sc, color, val, vis, img, res);
         return res;
     }
 };
