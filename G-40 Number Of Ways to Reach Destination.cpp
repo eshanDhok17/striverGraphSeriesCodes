0f21d3764ec2eedf28b0aa708a7f8f42a4509500
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2022-12-30 12:33:39 +0530

    second commit

--- G-40 Number Of Ways to Reach Destination.cpp
+++ G-40 Number Of Ways to Reach Destination.cpp
@@ -8,7 +8,7 @@
       maintain a distance[] as well as ways[] array where we count ways. Src can be reached only in 1 way that is by already being there.
       For others, we mark them while peforming dijkstra's.
 */
-
+// GFG
 typedef pair<int, int> pi;
 class Solution {
   public:
@@ -47,5 +47,41 @@ class Solution {
         }
         int ans = ways[n-1];
         return ans == 1e9? -1 : ans % mod;
+    }
+};
+
+// LeetCode
+class Solution {
+public:
+    typedef pair<long long, long long> pi;
+    int countPaths(int &n, vector<vector<int>>& roads) {
+        int mod = 1e9+7;
+        vector<pi> adj[n];
+        for(long long i=0; i<roads.size(); i++){
+            adj[roads[i][0]].push_back({roads[i][1], roads[i][2]});
+            adj[roads[i][1]].push_back({roads[i][0], roads[i][2]});
+        }
+        vector<long long> dist(n, 1e18), ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        priority_queue<pi, vector<pi>, greater<pi>> pq;
+        pq.push({0, 0});
+        while(!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            long long node = top.second, distWt = top.first;
+            for(auto &it : adj[node]) {
+                int adjNode = it.first, wt = it.second;
+                if(1LL*((long long)wt + (long long)distWt) < dist[adjNode]) {
+                    dist[adjNode] = 1LL*((long long)wt + (long long)distWt);
+                    pq.push({dist[adjNode], adjNode});
+                    ways[adjNode] = ways[node];
+                }
+                else if(1LL*((long long)wt + (long long)distWt) == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        return ways[n-1] % mod;
     }
 };
