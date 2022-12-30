49e44727d30664e78f35a94b0a8dc897667cac67
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2022-12-30 19:51:11 +0530

    only theory

---
+++ G-44 Minimum Spanning Tree (Theory).cpp
@@ -0,0 +1,7 @@
+/*
+  Definition of MST :
+      A tree in which we have N nodes and N-1 Edges and all nodes are
+      connected to each other directly or indirectly is a spanning tree.
+      The Spanning tree where sum of all edgeweights in that ST is minimum.
+      There can only be 1 MST and multiple ST.
+*/
