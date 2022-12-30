397469685a0acd67b0fb084321bac5fdba4f3a85
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2022-12-30 15:04:19 +0530

    Update G-42 Floyd Warshall.cpp

--- G-42 Floyd Warshall.cpp
+++ G-42 Floyd Warshall.cpp
@@ -4,6 +4,10 @@
     Intuition:
     How to detect -ve cycle? -> Distance from node to node is always 0, if it becomes < 0 we can say -ve cycle.
     Calculate Shortest Path Ultimate Matrix
+    Explain this before explaining this algorithm
+    1. why will dijkstra will not work?
+    2. How to deal with -Ve cycles?
+    3. Then introduce -> here comes this Floyd Warshall algo...
 */
 
 class Solution {
@@ -26,6 +30,14 @@ class Solution {
 	            for(int j=0; j<n; j++) {
 	                matrix[i][j] = min(matrix[i][j], (matrix[i][k] + matrix[k][j]));
 	            }
+	        }
+	    }
+		
+	    // To detect cycle
+            for(int i=0; i<n; i++) {
+	        if(matrix[i][i] < 0) {
+	            cout << "NEGATIVE CYCLE" << endl;
+                    return ;
 	        }
 	    }
 	    for(int i=0; i<n; i++) {
